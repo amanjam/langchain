7e1be4bf24558e5eb9c6c3d42c695c5e90843afa
Aman <amnkmrg@gmail.com>
2024-06-14 13:58:34 -0400

    name of package updated in the setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="langchain-monorepo",
+    name="langchain",
     version="0.0.1",
     packages=find_packages(where="libs"),
     package_dir={"": "libs"},
